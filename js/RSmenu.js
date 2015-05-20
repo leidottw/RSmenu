7191f0d98c4a959c9ab6baca22e895ce791bb28c
leidottw <leidottw@gmail.com>
2015-05-20 14:54:28 +0800

    add RSdropdown function

--- js/RSmenu.js
+++ js/RSmenu.js
@@ -1,5 +1,5 @@
 /**
- * RSMenu
+ * RSmenu
  * @author RayShen(leidottw@gmail.com)
  */
 
@@ -12,64 +12,49 @@
         this.top = offset.top;
         this.left = offset.left;
 
+        if(!menu) {
+            return;
+        }
+
         if(!menu.options) {
             menu.options = {};
         }
 
-        // setTriggerEvent
-        if(menu.options.active) {
-            setTriggerEvent.call(this);
-        } else {
-            menu.options.active = 'default';
-            setTriggerEvent.call(this);
-        }
+        // set trigger event
+        switch(menu.options.active) {
+            case 'leftClick':
+                $(this).on('click', function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
 
-        function setTriggerEvent() {
-            switch(menu.options.active) {
-                case 'leftClick': 
-                    $(this).on('click', function(e) {
-                        e.preventDefault();
-                        e.stopPropagation();
+                    that.mouseX = e.clientX;
+                    that.mouseY = e.clientY;
 
-                        that.mouseX = e.clientX;
-                        that.mouseY = e.clientY;
+                    render();
+                });
+                break;
+            default:
+                $(this).on('contextmenu', function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
 
-                        render();
-                    });
-                    break;
-                default:
-                    $(this).on('contextmenu', function(e) {
-                        e.preventDefault();
-                        e.stopPropagation();
+                    that.mouseX = e.clientX;
+                    that.mouseY = e.clientY;
 
-                        that.mouseX = e.clientX;
-                        that.mouseY = e.clientY;
-
-                        render();
-                    });
-            }
+                    render();
+                });
         }
 
         function render() {
             // execute on show
-            if(menu.options) {
-                if(menu.options.onShow) {
-                    menu.options.onShow();
-                }
+            if(menu.options.onShow) {
+                menu.options.onShow();
             }
 
-            that.$el = $('<div id="RSMenu">').appendTo('body');
+            that.$el = $('<div id="RSmenu">').appendTo('body');
 
-            if(menu.options && menu.options.debug) {
+            if(menu.options.debug) {
                 that.$el.addClass('debug');
-            }
-
-            // type
-            switch(menu.options.type) {
-                case 'dropdown':
-                    break;
-                default: // pop-up menu
-                    renderPopUpMenu(menu);
             }
 
             that.$el.on('click contextmenu', function(e) {
@@ -77,12 +62,12 @@
                 that.$el.remove();
 
                 // execute on show
-                if(menu.options) {
-                    if(menu.options.onClose) {
-                        menu.options.onClose();
-                    }
+                if(menu.options.onClose) {
+                    menu.options.onClose();
                 }
             });
+
+            renderPopUpMenu(menu);
         }
 
         function renderPopUpMenu(menu, parent) {
@@ -182,7 +167,12 @@
                 } else {
                     $menuInstance.css('bottom', 0);
                     if($menuInstance.outerHeight() > $(window).height()) {
-                        $menuInstance.height($(window).height()).jScrollPane({
+                        $menuInstance.css({
+                            'top': 0,
+                            'bottom': '',
+                            'margin-top': 0
+                        });
+                        $menuInstance.css('height', $(window).height()).jScrollPane({
                             autoReinitialise: true
                         });
                     }
@@ -200,7 +190,12 @@
                 } else {
                     $menuInstance.css('bottom', 0);
                     if($menuInstance.outerHeight() > $(window).height()) {
-                        $menuInstance.height($(window).height()).jScrollPane({
+                        $menuInstance.css({
+                            'top': 0,
+                            'bottom': '',
+                            'margin-top': 0
+                        });
+                        $menuInstance.css('height', $(window).height()).jScrollPane({
                             autoReinitialise: true
                         });
                     }
@@ -220,5 +215,249 @@
             }
         });
     };
+
+    $.fn.RSdropdown = function(menu) {
+        var that = this;
+
+        if(!menu) {
+            menu = {
+                data: []
+            };
+        }
+
+        if(!menu.options) {
+            menu.options = {};
+        }
+
+        // replace origin select menu
+        $(that).hide();
+        if($(that)[0].tagName === 'SELECT') {
+            $(that).find('option').each(function(key, option) {
+                menu.data.push({
+                    text: $(option).text(),
+                    val: $(option).val()
+                })
+            });
+        }
+
+        that.$dropdownHead = $('<div class="RSdropdownHead">').html(menu.data[0].text).attr('value', menu.data[0].val)/*.attr('title', menu.data[0].text)*/;
+
+        if(menu.options.width) {
+            that.$dropdownHead.css('width', menu.options.width);
+        }
+
+        $(that).after(that.$dropdownHead);
+
+        that.$dropdownHead.on('click', function() {
+            // execute on show
+            if(menu.options.onShow) {
+                menu.options.onShow();
+            }
+
+            that.$el = $('<div id="RSdropdown">').appendTo('body');
+
+            that.$dropdownHead.addClass('active');
+
+            if(menu.options.debug) {
+                that.$el.addClass('debug');
+            }
+
+            that.$el.on('click contextmenu', function(e) {
+                e.preventDefault();
+                that.$el.remove();
+
+                that.$dropdownHead.removeClass('active');
+
+                // execute on show
+                if(menu.options.onClose) {
+                    menu.options.onClose();
+                }
+            });
+
+            renderDropdown(menu);
+        });
+
+        function renderDropdown(menu, parent) {
+            that.trigger('createMenu', parent && parent.data('level') || 0);
+
+            var $menuInstance = $('<div class="menu">').appendTo(that.$el);
+
+            // 設定menu階層辨識參數
+            if(parent) {
+                $menuInstance[0].dataset.level = parent.data('level') + 1;
+                $menuInstance.data('parent', parent);
+                $menuInstance.addClass('submenu');
+            } else {
+                $menuInstance[0].dataset.level = 0;
+            }
+
+            // render item
+            menu.data.forEach(function(item) {
+                if(typeof item.hide === 'function') {
+                    if(item.hide()) return;
+                } else if(typeof item.hide === 'boolean') {
+                    if(item.hide) return;
+                }
+
+                if(item.divider) {
+                    var $itemInstance = $('<div class="divider">').appendTo($menuInstance);
+                } else {
+                    var $itemInstance = $('<div class="item">').html(item.text)/*.attr('title', item.text)*/.appendTo($menuInstance);
+
+                    if(item.class) {
+                        $itemInstance.addClass(item.class);
+                    }
+
+                    if(item.isTitle) {
+                        $itemInstance.data('isTitle', true);
+                        $itemInstance.addClass('isTitle');
+                    }
+
+                    // 設定menu階層辨識參數
+                    if(parent) {
+                        $itemInstance[0].dataset.level = parent.data('level') + 1;
+                        $itemInstance.data('parent', parent);
+                    } else {
+                        $itemInstance[0].dataset.level = 0;
+                    }
+
+                    // 若有子menu, 埋子menu設定參數
+                    if(item.menu) {
+                        $itemInstance.addClass('parentMenu');
+                        $itemInstance.data('menu', item.menu);
+                    }
+
+                    // 已選擇項目標記
+                    // 已選擇項目的parents標記
+                    if(that.$dropdownHead.attr('value') === item.val) {
+                        $itemInstance.addClass('selected');
+                    } else if(item.menu) {
+                        var searchArray = [item];
+                        
+                        while(searchArray.length) {
+                            search(searchArray[0], that.$dropdownHead.attr('value'));
+                            searchArray.shift();
+                        }
+
+                        function search(item, val) {
+                            if(item.menu) {
+                                $.each(item.menu.data, function(key, item) {
+                                    searchArray.push(item);
+                                });
+                            } else {
+                                if(item.val === val) {
+                                    $itemInstance.addClass('parentOfSelected');
+                                }
+                            }
+                        }
+                    }
+
+                    // 滑鼠事件
+                    $itemInstance.on('mouseenter', function(e) {
+                        // 若為父節點則render subment
+                        if(item.menu) {
+                            renderDropdown($itemInstance.data('menu'), $itemInstance);
+                        } else {
+                            that.trigger('createMenu', $itemInstance[0].dataset.level);
+                        }
+
+                        // 光標
+                        if(!$itemInstance.data('isTitle')) {
+                            $itemInstance.addClass('hover');
+                        }
+
+                        // 祖先光標
+                        that.$el.find('.item').removeClass('active');
+                        var tmp = parent;
+                        while(tmp) {
+                            tmp.addClass('active');
+                            tmp = tmp.data('parent');
+                        }
+                    }).on('mouseleave', function(e) {
+                        $itemInstance.removeClass('hover');
+                    }).on('click', function(e) {
+                        // 非title && 非父節點
+                        if(!item.isTitle && !item.menu) {
+                            $(that).trigger('change', item.val);
+                            that.$dropdownHead.html(item.text)/*.attr('title', item.text)*/.attr('value', item.val);
+                        }
+
+                        // 若有handler則執行
+                        if(item.handler) {
+                            item.handler();
+                        }
+                    });
+                }
+            });
+
+            // menu定位
+            if(parent) {
+                // 子menu定位
+                var parentOffset = parent.offset();
+
+                if(parentOffset.left + parent.outerWidth() + $menuInstance.outerWidth() <= $(window).width()) {
+                    $menuInstance.css('left', parentOffset.left + parent.outerWidth());
+                } else {
+                    $menuInstance.css('left', parentOffset.left - $menuInstance.outerWidth());
+                }
+
+                if(parentOffset.top + $menuInstance.outerHeight() + parseInt($menuInstance.css('marginTop')) <= $(window).height()) {
+                    $menuInstance.css('top', parentOffset.top - 1);
+                } else {
+                    $menuInstance.css('bottom', 0);
+                    if($menuInstance.outerHeight() > $(window).height()) {
+                        $menuInstance.css({
+                            'top': 0,
+                            'bottom': '',
+                            'margin-top': 0
+                        });
+                        $menuInstance.css('height', $(window).height()).jScrollPane({
+                            autoReinitialise: true
+                        });
+                    }
+                }
+            } else {
+                // root menu定位
+                var offset = that.$dropdownHead.offset();
+
+                $menuInstance.css('left', offset.left);
+                $menuInstance.css('width', that.$dropdownHead.outerWidth());
+
+                if(offset.top + that.$dropdownHead.outerHeight() + $menuInstance.outerHeight() <= $(window).height()) {
+                    $menuInstance.css('top', offset.top + that.$dropdownHead.outerHeight());
+                } else {
+                    if(offset.top > $(window).height() - offset.top) { // menu上方空間 > 下方空間
+                        $menuInstance.css('top', offset.top - $menuInstance.outerHeight());
+                        if($menuInstance.outerHeight() > offset.top) {
+                            $menuInstance.css('height', offset.top).jScrollPane({
+                                autoReinitialise: true
+                            });
+                        }
+                    } else {
+                        $menuInstance.css('top', offset.top + that.$dropdownHead.outerHeight());
+                        if($menuInstance.outerHeight() + offset.top > $(window).height()) {
+                            $menuInstance.css('height', $(window).height() - (offset.top + that.$dropdownHead.outerHeight())).jScrollPane({
+                                autoReinitialise: true
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        this.on('createMenu', function(e, level) {
+            if(that.$el) {
+                var allMenu = that.$el.find('.menu');
+
+                allMenu.each(function(index, menu) {
+                    if($(menu).data('level') > level) {
+                        $(menu).remove();
+                    }
+                });
+            }
+        });
+
+        return this;
+    }
 
 })(jQuery);
